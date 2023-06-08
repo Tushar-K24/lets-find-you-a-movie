68145326aef559124c198c3a11de1391287c135e
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2023-06-09 00:51:42 +0530

    add: login and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState("Login");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+
   return (
     <>
-      <Home />
+      {isAuthenticated ? (
+        <Home />
+      ) : (
+        <div className="auth-screen">
+          {currentForm == "Login" ? (
+            <Login toggleForm={toggleForm} />
+          ) : (
+            <Signup toggleForm={toggleForm} />
+          )}
+        </div>
+      )}
     </>
   );
 }
