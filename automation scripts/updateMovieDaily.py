7aea74364140ddb537ae0da0788ceb49a84d0d87
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2023-07-16 00:41:19 +0530

    rm: use of update movie api

--- automation scripts/updateMovieDaily.py
+++ automation scripts/updateMovieDaily.py
@@ -11,9 +11,6 @@ import utils.apiRequests as api
 import utils.dbRequests as db
 
 today = (date.today() - timedelta(days = 1)).strftime("%m_%d_%Y")
-log = open(f'./logs/log_{today}.txt', 'a')
-log.write(f"{datetime.now()} Starting...\n")
-
 url = dailyExportsUrl.replace("{today}", str(today))
 
 def extract_data_from_json_gz(url):
@@ -65,23 +62,37 @@ def add_movie_pipeline(movieID):
     movieData = response
     log.write(f"{datetime.now()} movie data retrieved for movieID:{movieID}\n")
     
-    #update movie data in db
-    log.write(f"{datetime.now()} updating movie data for movieID:{movieID}...\n")
-    updateBody = {param: movieData[param] for param in updateBodyParams if param in movieData.keys()}
-    status, response = db.updateMovie(movieID, updateBody)
+    # #update movie data in db
+    # log.write(f"{datetime.now()} updating movie data for movieID:{movieID}...\n")
+    # updateBody = {param: movieData[param] for param in updateBodyParams if param in movieData.keys()}
+    # status, response = db.updateMovie(movieID, updateBody)
+    # log.write(f"{datetime.now()} {response['message']}\n")
+    # if status == 200:
+    #     return {"updated": 1, "created": 0}
+    # elif status == 404:        
+    #     #add new movie in db
+    #     log.write(f"{datetime.now()} inserting movie data for movieID:{movieID}...\n")
+    #     status, response = db.addMovie(movieData)
+    #     log.write(f"{datetime.now()} {response['message']}\n")
+    #     if status == 201:
+    #         return {"updated": 0, "created": 1}
+    
+    # add new movie in db
+    log.write(f"{datetime.now()} inserting movie data for movieID:{movieID}...\n")
+    status, response = db.addMovie(movieData)
     log.write(f"{datetime.now()} {response['message']}\n")
-    if status == 200:
+    if status == 201:
+        return {"updated": 0, "created": 1}
+    if status == 202:
         return {"updated": 1, "created": 0}
-    elif status == 404:        
-        #add new movie in db
-        log.write(f"{datetime.now()} inserting movie data for movieID:{movieID}...\n")
-        status, response = db.addMovie(movieData)
-        log.write(f"{datetime.now()} {response['message']}\n")
-        if status == 201:
-            return {"updated": 0, "created": 1}
+    
     return None #if update/add movie unsuccessful    
 
 if __name__ ==  "__main__":
+
+    log = open(f'./logs/log_{today}.txt', 'a')
+    log.write(f"{datetime.now()} Starting...\n")
+    
     created, updated = 0, 0
     print("Downloading daily exports...")
     data = extract_data_from_json_gz(url)
@@ -91,7 +102,7 @@ if __name__ ==  "__main__":
         sys.exit(0)
     print("Download successful")
     print("Updating Database...")
-    pbar = tqdm(total=newMovies)
+    # pbar = tqdm(total=newMovies)
     for movie in data:
         if created>=newMovies:
             break
@@ -101,6 +112,11 @@ if __name__ ==  "__main__":
             print("Exiting...")
             sys.exit(1)
         created += response["created"]
-        if response["created"]>0:
-            pbar.update(1)
+        updated += response["updated"]
+        if (created + updated)%100 == 0:
+            print(f"Created: {created}, Updated: {updated}")
+        # if response["created"]>0:
+            # pbar.update(1)
     print("Movies Added Successfully")
+
+    log.close()
