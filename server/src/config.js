ea11ef37812c1c40e986de3e9d6f62a29dfa67d0
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2023-07-22 03:47:56 +0530

    add: hosted urls

--- server/src/config.js
+++ server/src/config.js
@@ -1,7 +1,7 @@
 accessTokenExpiry = "1h";
 adminAccessTokenExpiry = "30d";
 userInteractionHistory = 5;
-flaskBaseUrl = "http://127.0.0.1:5000";
+flaskBaseUrl = "https://recommendations-api-4l0d.onrender.com";
 
 module.exports = {
   accessTokenExpiry,
